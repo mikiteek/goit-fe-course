2e5aa78c621005ebb200cb33de3fd42165893ef9
mikiteek <mikiteek@gmail.com>
2020-06-23 15:49:28 +0300

    gulp-01-11 -> add 'del' npm package for deleting and rewrite 'dist' folder before build

--- glp-01-11/gulpfile.js
+++ glp-01-11/gulpfile.js
@@ -1,19 +1,21 @@
-const projectFolder = "build";
+const projectFolder = "dist";
 const sourseFolder = "src";
 
 const path = {
   build: {
     html: projectFolder + "/",
     css: projectFolder + "/css/",
+    js: projectFolder + "/js/",
     img: projectFolder + "/img/",
   },
   src: {
     html: [sourseFolder + "/*.html", "!" + sourseFolder + "/_*.html"], // читаем html все, исключаем с нижним подчеркиванием для построения в результат
     css: sourseFolder + "/scss/style.scss",
+    js: sourseFolder + "/js/script.js",
     img: sourseFolder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
   },
   watch: {
-    html: sourseFolder + "/**/*.html",
+    html: sourseFolder + "/**/*.html", // everything subdirs and watch all html-files
     css: sourseFolder + "/scss/**/*.scss",
     img: sourseFolder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
   },
@@ -23,7 +25,9 @@ const path = {
 const { src, dest } = require("gulp"),
   gulp = require("gulp"),
   brsync = require("browser-sync").create(),
-  fileinclude = require("gulp-file-include");
+  // work with different files and include to one html, build to one html
+  fileinclude = require("gulp-file-include"),
+  del = require("del");
 
 function browserSync(params) {
   brsync.init({
@@ -31,10 +35,10 @@ function browserSync(params) {
       baseDir: "./" + projectFolder + "/",
     },
     port: 3000,
-    notify: false,
+    notify: false, //turn off plagin notify
   });
 }
-
+//Function for work with html-files
 function html() {
   return src(path.src.html)
     .pipe(fileinclude())
@@ -42,11 +46,18 @@ function html() {
     .pipe(brsync.stream()); //обновим браузер вроде
 }
 
+
+
+// for live watching of html-partials
 function watchFiles(params) {
   gulp.watch([path.watch.html], html);
 }
+// deteting 'dist' forder before build project, and create new 'dist' folder
+function clean(params) {
+  return del(path.clean);
+}
 
-const build = gulp.series(html);
+const build = gulp.series(clean, html);
 // сценарий выполнения
 const watch = gulp.parallel(build, watchFiles, browserSync);
 
